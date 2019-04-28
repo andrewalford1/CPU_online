6fc115d07fea943120ccb07bd41313e0b26ffab7
Andrew Alford <35538694+andrewalford1@users.noreply.github.com>
2019-04-28 11:08:46 +0100

    Added more information to the IR

--- AndrewsCoolCPU/Assets/scripts/registers/InstructionRegister.cs
+++ AndrewsCoolCPU/Assets/scripts/registers/InstructionRegister.cs
@@ -1,4 +1,7 @@
-﻿/**
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/**
  * @brief   Class representing the IR.
  * @extends Register
  * @author  Andrew Alford
@@ -6,6 +9,10 @@
  * @version 1.1 - 29/03/2019
  */
 public class InstructionRegister : Register {
+
+    [SerializeField] private InputField opcodeInput = null;
+    [SerializeField] private InputField operandInput = null;
+
     public override BusControl.BUS_ROUTE RouteToPC      => BusControl.BUS_ROUTE.PC_IR;
     public override BusControl.BUS_ROUTE RouteToMAR     => BusControl.BUS_ROUTE.MAR_IR;
     public override BusControl.BUS_ROUTE RouteToMDR     => BusControl.BUS_ROUTE.MDR_IR;
@@ -16,4 +23,16 @@ public class InstructionRegister : Register {
     public override BusControl.BUS_ROUTE RouteToALUx    => BusControl.BUS_ROUTE.IR_ALUX;
     public override BusControl.BUS_ROUTE RouteToALUy    => BusControl.BUS_ROUTE.IR_ALUY;
     public override BusControl.BUS_ROUTE RouteToALUz    => BusControl.BUS_ROUTE.IR_ALUZ;
+
+    public new void Write(string content) {
+        contents.SetNumber(content);
+        opcodeInput.text = contents.GetHex().Substring(0, 2);
+        operandInput.text = contents.GetHex().Substring(2, 2);
+    }
+
+    public new void SetActive(bool active) {
+        this.active = active;
+        opcodeInput.interactable = active;
+        operandInput.interactable = active;
+    }
 }
